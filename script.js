fc07e8f9fef1b18028b0a79d4f1be3aeae7bdedc
Alexander Talberg <alexandermtalberg@gmail.com>
2020-01-07 11:54:01 -0700

    1

--- script.js
+++ script.js
@@ -5,15 +5,16 @@ var apiKey = '25f5253d7be788ad89940c40b9d3c859';
 cityName = 'denver';
 var queryURL = 'http://api.openweathermap.org/data/2.5/weather?id=524901&APPID=' + apiKey + '&q=' + cityName + '&units=imperial'
 var siteImg = 'cats'
-var searchTerm = $('#searchBar').val()
-
+var searchTerm = ''
+//weather 
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function (response) {
+  
     console.log(response)
 })
-
+//imgs
 var imgur =  {
     "url": `https://api.imgur.com/3/gallery/t/${siteImg}/`,
     "method": "GET",
@@ -27,7 +28,7 @@ var imgur =  {
   $.ajax(imgur).done(function (response) {
     console.log(response);
   });
-
+//search
   var searchResults = {
     "url": `https://developer.nps.gov/api/v1/parks?parkCode=&limit=10&stateCode=${searchTerm}&api_key=Pdcuwde0uYcGlFQwqha0ym4sxfIyAh5hvWQ7k4qV`,
     "method": "GET",
