237d930ac9d0780e3bc9067db9817e4df862a57d
cake <larosteguy@gmail.com>
2020-01-09 11:42:42 -0700

    fixed it bitch

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var lat = ''
 var long = ''
 var queryURL = 'http://api.openweathermap.org/data/2.5/weather?id=524901&APPID=' + apiKey + '&q=' + cityName + '&units=imperial'
 var siteImg = 'cats'
-var searchTerm; 
+var searchTerm;
 var parkCard;
 
 $.ajax({
@@ -31,14 +31,6 @@ var imgur = {
 $.ajax(imgur).done(function (response) {
   console.log(response);
 });
-//search
-
-
-
-
-
-
-
 
 
 $('#button').on('click', function (event) {
@@ -51,86 +43,105 @@ $('#button').on('click', function (event) {
     "timeout": 0,
   };
 
-
+  //search
   $.ajax(searchResults).done(function (response) {
-    
-    var latLong = response.data[0].latLong;
-  
-    
-    for (var i = 0; i <= 9 && i < response.data.length; i++) {var parkCard= $("<div class='card'> <div class='card-title'> <h3 class='park'>" + response.data[i].fullName + "</h3> </div> </div>")
 
-      parkCard.data("data", {fullName: response.data[i].fullName, 
-       description: response.data[i].description,
-       directionsInfo: response.data[i].directionsInfo,
-       entranceFees: response.data[i].entranceFees,
-       entrancePasses: response.data[i].entrancePasses});
+    var latLong = response.data[0].latLong;
+
+    //$("#").empty();
+    for (var i = 0; i <= 9 && i < response.data.length; i++) {
+      var parkCard = $(`
+    <div class="row clickable" >
+      <div class="col s5 offset-s4">
+          <div class="card">
+              <div class="card-image">
+                  <img src="npsLogo.png">
+                  </div>
+                  <div class="card-content">
+                  <span class="card-title">${response.data[i].fullName}</span>
+                  <p></p>
+              </div>
+              <div class="card-action">
+                  <a href="#">More Info</a>
+              </div>
+          </div>
+      </div>
+    </div>
+  `)
+
+      parkCard.data("data", {
+        fullName: response.data[i].fullName,
+        description: response.data[i].description,
+        directionsInfo: response.data[i].directionsInfo,
+        entranceFees: response.data[i].entranceFees,
+        entrancePasses: response.data[i].entrancePasses
+      });
       //  standardHours: response.data[i].operatingHours.standardHours,
       //  email: response.data[i].contacts.emailAddresses});
       var latLong = response.data[i].latLong
       var x = latLong.split(',')
       latFull = x[0]
       longFull = x[1]
-      
-      // console.log(lat)
-  
-  
-      //  weather(lat[1],long[1])
+
       console.log(parkCard)
-      $("#cards-go-here").append(parkCard);
-      
-    }// $(".wrapperDivForParks").append(park);
-  
+      $("#cards-generated").append(parkCard);
+
+    }
+
   })
 
 })
 
 
-  
 
-   
-    function latLongGen(i){
-      var searchResults = {
-        "url": `https://developer.nps.gov/api/v1/parks?parkCode=&limit=10&stateCode=${searchTerm}&api_key=Pdcuwde0uYcGlFQwqha0ym4sxfIyAh5hvWQ7k4qV`,
-        "method": "GET",
-        "timeout": 0,
-      };
-      $.ajax(searchResults).done(function (response){
-      var latLong = response.data[i].latLong
+
+
+function latLongGen(i) {
+  var searchResults = {
+    "url": `https://developer.nps.gov/api/v1/parks?parkCode=&limit=10&stateCode=${searchTerm}&api_key=Pdcuwde0uYcGlFQwqha0ym4sxfIyAh5hvWQ7k4qV`,
+    "method": "GET",
+    "timeout": 0,
+  };
+  $.ajax(searchResults).done(function (response) {
+    var latLong = response.data[i].latLong
     var x = latLong.split(',')
     latFull = x[0]
     longFull = x[1]
     lat = latFull.split(':')
     long = longFull.split(':')
-    
-      })
-    }
-function parkStorage(data){
+
+  })
+}
+
+
+function parkStorage(data) {
   localStorage.setItem("parkData", JSON.stringify(data));
 }
-
-$("#cards-generated").on("click", ".card", function() {
-  console.log($(this).data("data"))
-parkStorage($(this).data("data"));
+$("#cards-generated").on("click", ".card-action a", function(event) {
+  event.preventDefault();
+})
+$("#cards-generated").on("click", ".clickable", function (event) {
+  event.preventDefault();
+  
+  parkStorage($(this).data("data"));
+  window.location.replace('moreINFO.html')
 })
 
-function weather(lat,long){
+function weather(lat, long) {
 
   var apiKey = '25f5253d7be788ad89940c40b9d3c859';
-var queryURL = 'http://api.openweathermap.org/data/2.5/weather?id=524901&APPID=' + apiKey + '&lat=' + lat + '&lon=' + long + '&units=imperial'
+  var queryURL = 'http://api.openweathermap.org/data/2.5/weather?id=524901&APPID=' + apiKey + '&lat=' + lat + '&lon=' + long + '&units=imperial'
 
 
-$.ajax({
-url: queryURL,
-method: "GET"
-}).then(function (response) {
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
 
-console.log(response)
-})
+    console.log(response)
+  })
 
 }
 
 
-// later, when the user clicks on one of the park/location 'more info' links, write code to grab the .data 
-// function parkStorage(){
-//     localStorage.setItem('parkData', JSON.stringify(parkCard));
-// }
+
