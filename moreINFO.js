9f6943ebc772f394d94eb41258dba5e18f86dc2c
Alexander Talberg <alexandermtalberg@gmail.com>
2020-01-10 10:14:11 -0700

    changed the search term to help them results come back better? again

--- moreINFO.js
+++ moreINFO.js
@@ -36,7 +36,7 @@ function navigateMoreINFO(){
   function weather(lat, long) {
 
     var apiKey = '25f5253d7be788ad89940c40b9d3c859';
-    var queryURL = 'http://api.openweathermap.org/data/2.5/weather?id=524901&APPID=' + apiKey + '&lat=' + lat + '&lon=' + long + '&units=imperial'
+    var queryURL = 'https://api.openweathermap.org/data/2.5/weather?id=524901&APPID=' + apiKey + '&lat=' + lat + '&lon=' + long + '&units=imperial'
   
   
     $.ajax({
